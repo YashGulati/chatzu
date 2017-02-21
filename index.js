9a0a8983b2d7600a0951de5cdd43f5658485b64a
yash <yashgulati.g1@gmail.com>
2017-02-21 23:15:51 +0530

     

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var app = require('express')()
-
-app.get('*', (req,res) => {
-  res.send('Hello World')
-})
-
-app.listen(80, function(){
-    console.log("Server Listening on port 80...");
-});
+// var app = require('express')()
+//
+// app.get('*', (req,res) => {
+//   res.send('Hello World')
+// })
+//
+// app.listen(80, function(){
+//     console.log("Server Listening on port 80...");
+// });
+console.log('Working');
