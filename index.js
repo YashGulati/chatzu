65adcb4e6585cfda731f4b8e3940c986e3e4da38
yash <yashgulati.g1@gmail.com>
2017-02-22 12:20:56 +0530

     

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('*', (req,res) => {
   res.send('Hello World')
 })
 
-app.listen(80, function(){
+app.listen(80 || 90, function(){
     console.log("Server Listening on port 80...");
 });
 console.log('Working');
